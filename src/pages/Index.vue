fdf5d09923d9f2e6ec7a67f0605de2fada8b8c43
krcalcn <alicn.karaca@gmail.com>
2020-07-29 11:00:32 +0300

    few things

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+   <q-list bordered separator>
+      <q-item clickable v-ripple v-for="task in tasks" v-bind:key="task.id">
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+          <q-item-label caption>
+            {{task.dueDate}}
+          </q-item-label>
+          <q-item-label caption>
+            {{task.dueTime}}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+   </q-list>
   </q-page>
 </template>
 
 <script>
+
 export default {
-  name: 'PageIndex',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go to shop',
+          dueDate: '11/08/2020',
+          dueTime: '10:00',
+        },
+        {
+          id: 2,
+          name: 'Get bananas',
+          dueDate: '12/08/2020',
+          dueTime: '11:00',
+        },
+        {
+          id: 3,
+          name: 'Get apples',
+          dueDate: '13/08/2020',
+          dueTime: '14:30',
+        },
+      ],
+    };
+  },
+  methods: {
+    completeTask(a) {
+      console.log(a);
+    },
+  },
 };
 </script>
---
+++ src/pages/Index2.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-page padding>
+    <button
+    @click="counter++">
+    {{ counter }}
+    </button>
+    <div class="triangle-right" @click="triangleAction">
+    </div>
+    <input v-model="counter" v-autofocus>
+    <h5
+    v-for="obj in object"
+    v-bind:key="obj.id"
+    class="shadow-6 q-pa-md text-center">
+      {{obj.name}}
+      <br>
+      ID: <input
+      v-model="obj.id"
+      disabled
+      style="width:40px;"
+      class="q-mr-md">
+      <br>
+      <input
+      v-model="obj.name"
+      @keyup="handleKeyup">
+    </h5>
+    <h6>
+      {{ messageUppercase }}
+    </h6>
+    <h6>
+      {{ object | messageLowercase }}
+    </h6>
+  </q-page>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      // todos: [],
+      counter: 0,
+      object: [
+        {
+          id: 1,
+          name: 'Object 1',
+        },
+        {
+          id: 2,
+          name: 'Object 2',
+        },
+      ],
+    };
+  },
+  computed: {
+    messageUppercase() {
+      const names = [];
+      this.object.map((ob) => {
+        names.push(ob.name.toUpperCase());
+        return null;
+      });
+      console.log('computed');
+      return names;
+    },
+  },
+  methods: {
+    handleKeyup() {
+      console.log('keyup');
+    },
+    triangleAction(e) {
+      console.log(e.target);
+    },
+    // fetchData() {
+    //   const a = new Promise((resolve) => {
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //       .then((response) => {
+    //         this.todos = response.data;
+    //         resolve(response.data);
+    //       })
+    //       .catch(function (error) {
+    //         this.answer = `Error! Could not reach the API.  ${error}`;
+    //       });
+    //   });
+    //   return a;
+    // },
+  },
+  filters: {
+    messageLowercase(value) {
+      const names = [];
+      value.map((ob) => {
+        names.push(ob.name.toLowerCase());
+        return null;
+      });
+      console.log('filter');
+      return names;
+    },
+  },
+  // mounted() {
+  //   this.fetchData();
+  // },
+  directives: {
+    autofocus: {
+      inserted(el) {
+        el.focus();
+      },
+    },
+  },
+};
+</script>
+<style>
+.triangle-right {
+  width: 0;
+  height: 0;
+  border-top: 25px solid transparent;
+  border-bottom: 25px solid transparent;
+  border-left: 50px solid black;
+  cursor: pointer;
+}
+</style>
